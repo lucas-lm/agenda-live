fef2077f96edba579c77891c9cdcc9f78bd800d5
Lucas <lucas-m@outlook.com>
2022-02-20 23:43:31 -0300

    add getLives method to init list lives component

--- src/app/views/home/list-live/list-live.component.ts
+++ src/app/views/home/list-live/list-live.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Live } from 'src/app/shared/models/Live.model';
+import { LiveService } from 'src/app/shared/services/live.service';
 
 @Component({
   selector: 'app-list-live',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListLiveComponent implements OnInit {
 
-  constructor() { }
+  livesPrevious!: Live[];
+  livesNext!: Live[];
+
+  constructor(
+    public liveService: LiveService
+  ) { }
 
   ngOnInit(): void {
+    this.getLives()
+  }
+
+  getLives() {
+    this.liveService.getLivesWithFlag('previous').subscribe(data => {
+      this.livesPrevious = data.content;
+      console.log(this.livesPrevious)
+    })
+
+    this.liveService.getLivesWithFlag('next').subscribe(data => {
+      this.livesNext = data.content;
+      console.log(this.livesNext)
+    })
   }
 
 }
