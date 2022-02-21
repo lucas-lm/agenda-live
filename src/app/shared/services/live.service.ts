912cceb9454d38703abc762956545d4d6a01f423
Lucas <lucas-m@outlook.com>
2022-02-21 00:03:01 -0300

    add mock api response

--- src/app/shared/services/live.service.ts
+++ src/app/shared/services/live.service.ts
@@ -3,12 +3,20 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ResponsePageable } from '../models/ResponsePageable.model';
 
+interface apiUrl {
+  previous: string,
+  next: string
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class LiveService {
 
-  apiUrl = '';
+  apiUrl: apiUrl = {
+    'previous': './assets/lives/previousLives.json',
+    'next': './assets/lives/nextLives.json'
+  };
 
   httpOptions = {
     headers: new HttpHeaders({
@@ -20,7 +28,7 @@ export class LiveService {
     private httpClient: HttpClient
   ) { }
 
-  public getLivesWithFlag(flag: string): Observable<ResponsePageable> {
-    return this.httpClient.get<ResponsePageable>(`${this.apiUrl}?flag=${flag}`)
+  public getLivesWithFlag(flag: "previous" | "next"): Observable<ResponsePageable> {
+    return this.httpClient.get<ResponsePageable>(this.apiUrl[flag])
   }
 }
