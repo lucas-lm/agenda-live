842540da0000512a01087ef3ece34f361378111f
Lucas <lucas-m@outlook.com>
2022-02-22 00:32:31 -0300

    add content to live cards

--- src/app/views/home/list-live/list-live.component.ts
+++ src/app/views/home/list-live/list-live.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { Live } from 'src/app/shared/models/Live.model';
 import { LiveService } from 'src/app/shared/services/live.service';
 
@@ -13,7 +14,8 @@ export class ListLiveComponent implements OnInit {
   livesNext!: Live[];
 
   constructor(
-    public liveService: LiveService
+    public liveService: LiveService,
+    public sanitazer: DomSanitizer
   ) { }
 
   ngOnInit(): void {
@@ -23,11 +25,19 @@ export class ListLiveComponent implements OnInit {
   getLives() {
     this.liveService.getLivesWithFlag('previous').subscribe(data => {
       this.livesPrevious = data.content;
+      this.livesPrevious.forEach(live => {
+        live.urlSafe = this.sanitazer
+          .bypassSecurityTrustResourceUrl(live.liveLink.replace('watch?v=', 'embed\/'));
+      });
       console.log(this.livesPrevious)
     })
 
     this.liveService.getLivesWithFlag('next').subscribe(data => {
       this.livesNext = data.content;
+      this.livesNext.forEach(live => {
+        live.urlSafe = this.sanitazer
+          .bypassSecurityTrustResourceUrl(live.liveLink.replace('watch?v=', 'embed\/'))
+      })
       console.log(this.livesNext)
     })
   }
